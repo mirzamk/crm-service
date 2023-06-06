4c89940e4f32647f40c7f208af09e736f4bb044d
User <mirza.khusni@gmail.com>
2023-06-06 08:00:12 +0700

    build:add database

---
+++ utils/db/gorm.go
@@ -0,0 +1,17 @@
+package db
+
+import (
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"log"
+)
+
+func GormMysql() *gorm.DB {
+	db, err := gorm.Open(mysql.Open("root:@tcp(127.0.0.1:3306)/crm-service?charset=utf8mb4&parseTime=True&loc=UTC"),
+		&gorm.Config{})
+	if err != nil {
+		log.Println("gorm.open", err)
+	}
+	return db
+
+}
